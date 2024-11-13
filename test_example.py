9f851271686bc1218a42336ecc3ded196de19fe0
Helen Yanushevskaya <qahelping@gmail.com>
2024-11-13 19:06:49 +0300

    - add project

--- test_example.py
+++ test_example.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 config = Config(
     apiUrl='http://localhost:4200',
-    project='Default project',
+    project='ca36ff18-7ea7-407b-bc5a-0411ef819f29',
     apiKey='DEFAULTUSERAPIKEYTOBECHANGED',
     ciBuildId='commit_sha',
     branchName='develop',
@@ -20,8 +20,51 @@ def test_visual_regression_tracker():
     with vrt:
         try:
             driver.get("https://www.python.org/psf-landing/")
+            driver.set_window_size(width=800, height=600)
             vrt.track(TestRun(
                 name='psf-landing',
+                imageBase64=driver.get_screenshot_as_base64(),
+                diffTollerancePercent=0,
+                os='Mac',
+                browser='Chrome',
+                viewport='800x600',
+                device='PC',
+            ))
+        finally:
+            driver.quit()
+
+def test_visual_regression_tracker2():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    vrt = VisualRegressionTracker(config)
+
+    with vrt:
+        try:
+            driver.get("https://github.com/sponsors")
+            driver.set_window_size(width=800, height=600)
+            vrt.track(TestRun(
+                name='github',
+                imageBase64=driver.get_screenshot_as_base64(),
+                diffTollerancePercent=0,
+                os='Mac',
+                browser='Chrome',
+                viewport='800x600',
+                device='PC',
+            ))
+        finally:
+            driver.quit()
+
+
+def test_visual_regression_tracker3():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    vrt = VisualRegressionTracker(config)
+
+    with vrt:
+        try:
+            driver.get("https://candymapper.com/")
+            # driver.get("https://candymapperr2.com")
+            driver.set_window_size(width=800, height=600)
+            vrt.track(TestRun(
+                name='github',
                 imageBase64=driver.get_screenshot_as_base64(),
                 diffTollerancePercent=0,
                 os='Mac',
